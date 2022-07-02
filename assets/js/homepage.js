ee4dc524fc7dbaad6063b6162cc20653cf17194c
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-07-02 16:00:48 -0500

    submitting a push

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,65 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  console.log(event);
+};
+
+// get value from input element
+var username = nameInputEl.ariaValueMax.trim();
+
+
+
 var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+};
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      displayRepos(data, user);
     });
-  };
+  });
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+
+var displayRepos = function(repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
+
+  // clar old content
+  repoContainerEl.textcontent = "";
+  repoSearchTerm.textContent = searchTerm;
+
+  // loop over repos
+  for (var i = 0; i < repos.length; i++) {
+    //format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+    // create a container for each repo
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+
+    // append to container
+    repoEl.appendChild(titleEl);
+
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+
